e29651ee0efadf43cd82ea6e95e3752ad3d197d5
Tameka Roberson <TTRoberson@student.fullsail.edu>
2024-06-03 15:18:41 -0400

    Task 2 Completed

---
+++ Task-2.py
@@ -0,0 +1,10 @@
+# Set Operation in Data Analysis
+
+customer_ids = ["C001", "C002", "C003", "C002", "C001", "C004"]
+
+duplicate_removal = set()
+
+duplicate_removal.update(customer_ids)
+
+print(duplicate_removal)
+
